d6f26e6d42cdeb5e04b3fe3e990656c100a051ab
i.wind <ig-wind@mail.ru>
2013-02-02 18:16:11 +0400

    Check for some mistake

--- docs/noddy.py
+++ docs/noddy.py
@@ -10,3 +10,12 @@ nd.first = "Harry"
 nd.last = "Trueman"
 
 print nd.name()
+
+# Noddy objects can participate in cycles. This is pretty silly,
+# but it gives us an excuse to add support for the cyclic-garbage
+# collector to the Noddy example.
+n = noddy.Noddy()
+l = [n]
+n.first = l
+
+print n.name()
