710373b61bdf07824925d9e6186bbf9b58b78e38
i.wind <ig-wind@mail.ru>
2013-02-03 20:22:26 +0400

    Adding constructor to class

--- boost/hello.cpp
+++ boost/hello.cpp
@@ -3,6 +3,7 @@ using namespace boost::python;
 
 struct World
 {
+    World(std::string msg): msg(msg) {}  // constructor
     void set(std::string msg) { this->msg = msg; }
     std::string greet() { return msg; }
     std::string msg;
@@ -10,7 +11,7 @@ struct World
 
 BOOST_PYTHON_MODULE(hello)
 {
-    class_<World>("World")
+    class_<World>("World", init<std::string>())
         .def("greet", &World::greet)
         .def("set", &World::set)
     ;
--- boost/hello.py
+++ boost/hello.py
@@ -1,5 +1,7 @@
-import hello
+from hello import World
 
-world = hello.World()
+world = World("Hello guys!")
+print world.greet()
+
 world.set("Some greeting!")
 print world.greet()
