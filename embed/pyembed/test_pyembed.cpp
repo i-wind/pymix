8ee7ce0543410f37c7dda4be3587b2c8a8d46fe1
i.wind <ig-wind@mail.ru>
2013-02-03 23:15:23 +0400

    More tests on pyembed

--- embed/pyembed/test_pyembed.cpp
+++ embed/pyembed/test_pyembed.cpp
@@ -1,41 +1,96 @@
 #include <iostream>
+
 #include "pyembed.h"
 
 using namespace pyembed; // for brevity
 
+// our unit test function
+static void run_test(int argc, char** argv);
+
 int main(int argc, char** argv)
 {
-    try // always check for Python_exceptions
+    run_test(argc, argv);
+    return 0;
+}
+
+void run_test(int argc, char** argv)
+{
+    try
     {
-        // Create an instance of the interpreter.
+        // Ok. Now we can create an instance of the interprter itself.
         Python py(argc, argv);
 
+        // to check if the python interpreter is available and working,
+        // execute a simple python statement.
+        py.run_string("print 'Hello world from Python!'");
+
+        // execute the script in a file
+        py.run_file("hello.py");
+      
         // Load the test module into the interpreter
-        py.load("test");           
+        py.load("test");
 
-        // value returned by python is stored here    
-        long ret = 0; 
+        // Call a simple function, that takes no arguments
+        // and returns nothing      
+        py.call("simple");
 
-        // our argument list that maps string values 
-        // to their python types    
-        Arg_map args; 
 
-        // fill up argument strings and their types.
-        args["10"] = Py_long;
-        args["20"] = Py_long;
+        Arg_map args; // our argument list that maps string values 
+                      // to their python types     
 
-        // make the call
-        py.call("multiply", args, ret);
+        // Call a function that takes two long arguments and returns
+        // a long      
+        {
+            long ret = 0; // value returned by python is stored here
+            args["10"] = Py_long;
+            args["20"] = Py_long;
+            py.call("multiply", args, ret);
+            std::cout << ret << '\n';
+            args.clear();
+        }
 
-        // will print 200
-        std::cout << ret << '\n'; 
+        // call a function a takes a string and just echoes it
+        {
+            std::string ret = "";
+            args["Hello from C++"] = Py_string;
+            py.call("echo", args, ret);
+            std::cout << ret << '\n';
+            args.clear();
+        }
 
-        return 0;
+        // call a function that returns a list
+        {
+            String_list ret;
+            py.call("names_as_list", args, ret);
+            size_t sz = ret.size();
+            for (size_t i=0; i<sz; i++)
+                std::cout << ret[i] << '\t';
+            std::cout << '\n';
+        }
+
+        // call a function that returns a tuple
+        {
+            String_list ret;
+            py.call("names_as_tuple", args, ret);
+            size_t sz = ret.size();
+            for (size_t i=0; i<sz; i++)
+                std::cout << ret[i] << '\t';
+            std::cout << '\n';
+        }
+
+        // call a function that returns a dictionary
+        {
+            String_map ret;
+            py.call("config", args, ret);
+            String_map::const_iterator it;
+            for (it = ret.begin(); it != ret.end(); it++)
+                std::cout << it->first << ':' << it->second << '\t';
+            std::cout << '\n';
+        }
+
     }
     catch (Python_exception ex)
     {
-        std::cout << ex.what();
+        std::cout << ex.what() << '\n';
     }
-
-    return 0;
 }
